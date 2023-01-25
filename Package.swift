f76b593f6c0fece0e045252192910a614edfb6fc
Cristian Barril <cristian.barril@muuvlabs.com>
2023-01-25 11:08:33 -0300

    Package updated

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "WorkoutsPlayerWrapper",
-            targets: ["WorkoutsPlayerUI", "WorkoutsPlayerCoreInternal"]
+            targets: ["WorkoutsPlayerUI"]
         )
     ],
     dependencies: [
@@ -22,17 +22,12 @@ let package = Package(
     targets: [
         .target(
             name: "WorkoutsPlayerWrapper",
-            dependencies: ["WorkoutsPlayerUI", "WorkoutsPlayerCoreInternal"]
+            dependencies: ["WorkoutsPlayerUI"]
         ),
         .binaryTarget(
             name: "WorkoutsPlayerUI",
             url: "https://github.com/muuvlabs/muuv-utilities-ios/raw/master/WorkoutsPlayerUI/0.0.1/WorkoutsPlayerUI-0.0.1.xcframework.zip",
             checksum: "c5c896187745a1422cf3bb9055f340bc6933fe7351f62ca709e35a5940af6e5f"
-        ),
-        .binaryTarget(
-            name: "WorkoutsPlayerCoreInternal",
-            url: "https://github.com/muuvlabs/muuv-utilities-ios/raw/master/WorkoutsPlayerCoreInternal/0.0.1/WorkoutsPlayerCoreInternal-0.0.1.xcframework.zip",
-            checksum: "1569bb5ed84dcd9c90b39226fc8899d636f767cf9c1f8524f0bc4778e500abe7"
         )
     ]
 )
